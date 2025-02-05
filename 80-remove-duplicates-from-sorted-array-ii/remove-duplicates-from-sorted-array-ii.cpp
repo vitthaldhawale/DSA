6cbdbfa90dec54351d8a73c8a90483cf2cabbf90
vitthaldhawale <dhawalevitthal7@gmail.com>
2025-02-05 14:59:16 +0530

    Time: 11 ms (8.20%) | Memory: 20.1 MB (5.64%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -1,34 +1,59 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int n=nums.size();
-        if(n<=2) return n;
-        vector<int> ans;
-        int i=0;
-        int j=0;
-        int cnt=0;
-        while(j<n)
-        {
-            if(nums[i]==nums[j])
-            {
-                cnt++;
-                j++;
-                if(cnt<=2)
-                {
-                    ans.push_back(nums[i]);
-                }
-            }
-            else
-            {
-                i=j;
-                cnt=0;
-            }
-        }
-        for(int i=0;i<ans.size();i++)
-        {
-            nums[i]=ans[i];
-        }
-        return ans.size();
+        // int n=nums.size();
+        // if(n<=2) return n;
+        // vector<int> ans;
+        // int i=0;
+        // int j=0;
+        // int cnt=0;
+        // while(j<n)
+        // {
+        //     if(nums[i]==nums[j])
+        //     {
+        //         cnt++;
+        //         j++;
+        //         if(cnt<=2)
+        //         {
+        //             ans.push_back(nums[i]);
+        //         }
+        //     }
+        //     else
+        //     {
+        //         i=j;
+        //         cnt=0;
+        //     }
+        // }
+        // for(int i=0;i<ans.size();i++)
+        // {
+        //     nums[i]=ans[i];
+        // }
+        // return ans.size();
+        int n = nums.size();
+        if (n <= 2) return n; // If the array has 2 or fewer elements, no need to process
 
+        vector<int> ans; // Initialize the result vector
+        int cnt = 1; // Counter for duplicates
+        ans.push_back(nums[0]); // Add the first element to the result
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1]) {
+                cnt++; // Increment duplicate count
+            } else {
+                cnt = 1; // Reset duplicate count for a new element
+            }
+
+            // If the current element has appeared at most twice, add it to the result
+            if (cnt <= 2) {
+                ans.push_back(nums[i]);
+            }
+        }
+
+        // Copy the result back to the original array
+        for (int i = 0; i < ans.size(); i++) {
+            nums[i] = ans[i];
+        }
+
+        return ans.size(); // Return the new length of the array
     }
 };
