12a50e75cf78fd1339442b26947ac3024b10a245
vitthaldhawale <dhawalevitthal7@gmail.com>
2025-02-27 00:59:14 +0530

    Time: 1 ms (11.20%) | Memory: 27.3 MB (1.71%) - LeetSync

---
+++ 3723-sum-of-good-numbers/sum-of-good-numbers.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int sumOfGoodNumbers(vector<int>& nums, int k) {
+        int sum=0;
+        int n=nums.size();
+        if(nums[0]>nums[0+k]) sum+=nums[0];
+        if(nums[n-1]>nums[n-1-k]) sum+=nums[n-1];
+        int i=1;
+        while(i<n-1)
+        {
+            if(i-k>=0 && i+k<=n-1)
+            {
+                if(nums[i-k]< nums[i] && nums[i]>nums[i+k])
+                {
+                    sum+=nums[i];
+                }
+            }
+            else if(i-k<0 && i+k>n-1)
+            {
+                sum+=nums[i];
+            }
+            else if(i-k>=0)
+            {
+                if(nums[i]>nums[i-k]) sum+=nums[i];
+            }
+            else if(i+k<=n-1)
+            {
+                if(nums[i+k]<nums[i]) sum+=nums[i];
+            }
+            i++;
+        }
+        return sum;
+    }
+};
