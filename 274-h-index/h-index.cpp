ea61fd1ad8a0a9406aba93747cfe5700e23dada2
vitthaldhawale <dhawalevitthal7@gmail.com>
2025-03-03 23:43:32 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (41.61%) - LeetSync

---
+++ 274-h-index/h-index.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+int func(vector<int>& arr,int mid)
+{
+    int cnt=0;
+    for(int i=0;i<arr.size();i++)
+    {
+        if(mid<=arr[i]) cnt++;
+    }
+    return cnt;
+}
+    int hIndex(vector<int>& citations) {
+        int cnt=0;
+        int n=citations.size();
+        int s=0;
+        int e=n;
+        int mid=0;
+        int cntfunc=0;
+        while(s<=e)
+        {
+            mid=(s+e)/2;
+            cnt=func(citations,mid);
+            if(cnt>=mid)
+            {
+                s=mid+1;
+                cntfunc=mid;
+            } 
+            else e=mid-1;
+        }
+        
+    return cntfunc;
+    }
+};
