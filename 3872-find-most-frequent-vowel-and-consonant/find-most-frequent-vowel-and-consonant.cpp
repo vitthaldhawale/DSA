479607572e5c9f8166498c5ae6b9a6aebf05974a
vitthaldhawale <dhawalevitthal7@gmail.com>
2025-09-13 14:07:41 +0530

    Time: 3 ms (37.49%) | Memory: 10 MB (30.82%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
@@ -7,9 +7,6 @@ public:
         for(int i=0;i<s.size();i++)
         {
             map[s[i]-'a']++;
-        }
-        for(int i=0;i<s.size();i++)
-        {
             if(s[i]=='a' || s[i]=='e'|| s[i]=='i'|| s[i]=='o'|| s[i]=='u')
             {
                 maxvowel = max(maxvowel,map[s[i]-'a']);
