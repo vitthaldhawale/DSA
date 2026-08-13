3f63c208daab220bad012c68610d2b8b97c0b978
vitthaldhawale <dhawalevitthal7@gmail.com>
2026-08-13 15:11:10 +0530

    Time: 2 ms (35.95%) | Memory: 10.3 MB (23.57%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-
-        string ans="";
-        for(char c:s)
-        {
-            if(isalnum(c))
-            {
-                ans+=tolower(c);
+        int n = s.size();
+        string st = "";
+        for(int i=0;i<n;i++){
+            if((s[i]>='A' && s[i]<='Z') || (s[i]>='a' && s[i]<='z') || (s[i]>='0' && s[i]<='9')){
+                st += s[i];
             }
         }
-        int l=0,r=ans.size()-1;
-        while(l<=r)
-        {
-            if(ans[l]==ans[r])
-            {
-                l++;
-                r--;
-            }
-            else return false;
+        int l = 0;
+        int r = st.size()-1;
+        while(l<=r){
+            if(tolower(st[l])!=tolower(st[r])) return false;
+            l++;
+            r--;
         }
-    return true;
+        return true;
     }
 };
